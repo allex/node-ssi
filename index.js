01aa2264c703ecdad48b67f1a4530bad01dafee5
Allex Wang <allex.wxn@gmail.com>
2015-10-14 14:49:56 +0800

    fix ssi nested path includes.

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
+"use strict";
 
 var SSI = require("./lib/SSI");
 
-(function() {
-	"use strict";
+var ssi = function(inputDirectory, outputDirectory, matcher) {
+    this.parser = new SSI(inputDirectory, outputDirectory, matcher);
+};
 
-	var ssi = function(inputDirectory, outputDirectory, matcher) {
-		this.parser = new SSI(inputDirectory, outputDirectory, matcher);
-	};
+ssi.prototype = {
+    compile: function() {
+        return this.parser.compile();
+    },
+    parse: function(filename, contents) {
+        return this.parser.parse(filename, contents);
+    }
+};
 
-	ssi.prototype = {
-		compile: function() {
-			return this.parser.compile();
-		},
-		parse: function(filename, contents) {
-			return this.parser.parse(filename, contents);
-		}
-	};
-
-	module.exports = ssi;
-})();
+module.exports = ssi;
